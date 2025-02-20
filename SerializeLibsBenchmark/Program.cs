0ef6e1cdb82bd0f25ae2d4a7ce74307a450917fe
Isak Pao <windperson@gmail.com>
2025-02-20 18:56:10 +0800

    Workable multiple json.net dlls in BenchmarkDotNet crank project on both Windows & Linux

--- SerializeLibsBenchmark/Program.cs
+++ SerializeLibsBenchmark/Program.cs
@@ -1,6 +1,4 @@
-﻿using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Jobs;
-using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Running;
 
 namespace SerializeLibsBenchmark;
 
@@ -8,9 +6,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        var config = ManualConfig.Create(DefaultConfig.Instance)
-            .AddJob(Job.Default.DontEnforcePowerPlan().AsDefault());
-
-        BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
+        BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
     }
 }
---
+++ libs/nupkgs/README.md
@@ -0,0 +1,3 @@
+# Local Nuget packages 
+
+This folder contains the ClassLib Alpha, Beta, Gamma Nuget packages that will be used by the project.
